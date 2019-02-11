3933ea9a37ec6ed1a53e6584b521fd7f630e60dc
Scott Gustafson <scott@secret>
2019-02-10 22:19:59 -0700

    Made mission1 a little cleaner.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # reaktor
 Fun Puzzles
+
+These are some interesting puzzles that are pretty easy to solve.
+https://tracking-game.reaktor.com/
--- mission1.pl
+++ mission1.pl
@@ -1,18 +1,30 @@
 #!/usr/bin/perl
+#
 # Copyright 2019 R. Scott Gustafson
+#
+# https://tracking-game.reaktor.com/signal/vs/noise/play
+# Find the message in the text.
+# 16 bytes long and contains no repeating characters
 
-while (<>) {
+use MIME::Base64;
+
+open (MESSAGE, "< mission1.txt")
+    or die "Couldn't open mission1.txt for reading: $!\n";
+
+while (<MESSAGE>) {
     chomp;
     $line = $_;
     for ($x = 0; $x < length $line; $x++) {
         $substring = substr $line, $x, 16;
         %seen = ( );
         @uniq = grep { ! $seen{$_} ++ } unpack('C*' , $substring);
-        if ($#uniq == 15) {
-            print $substring . "\n";
+        if ($#uniq == (16 - 1)) {
+            print decode_base64($substring) . "\n";
+            last;
         }
     }
 }
 
-# cat message.txt | ./mission1.pl - | openssl enc -d -base64
+close (MESSAGE);
+
 # Curtisisland
